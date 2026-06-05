f570964b9e2f32f32bacf0904035dea8f8facf1e
Reneghis1999 <viceprojects19@gmail.com>
2026-06-05 17:29:31 +0000

    feat: implement JWT authentication system

--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import generics, permissions
+from .serializers import RegisterSerializer
+from .models import User
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+
+class ProfileView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "id": request.user.id,
+            "username": request.user.username,
+            "email": request.user.email
+        })
