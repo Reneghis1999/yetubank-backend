b165ab54bf96f0e8227644eda401a230163db067
Reneghis1999 <viceprojects19@gmail.com>
2026-06-08 19:33:11 +0000

    feat: create transaction history API

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from .models import Transaction
 
 
 class TransferSerializer(serializers.Serializer):
@@ -7,3 +8,24 @@ class TransferSerializer(serializers.Serializer):
         max_digits=12,
         decimal_places=2
     )
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    sender = serializers.CharField(
+        source='from_account.user.username'
+    )
+
+    receiver = serializers.CharField(
+        source='to_account.user.username'
+    )
+
+    class Meta:
+        model = Transaction
+        fields = [
+            'id',
+            'sender',
+            'receiver',
+            'amount',
+            'transaction_type',
+            'created_at'
+        ]
--- transactions/urls.py
+++ transactions/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import TransferView
+from .views import (
+    TransferView,
+    TransactionListView
+)
 
 urlpatterns = [
     path('transfer/', TransferView.as_view()),
+
+    path('transactions/', TransactionListView.as_view()),
 ]
